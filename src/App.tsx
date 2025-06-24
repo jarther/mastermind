cd290792f9c8ccd476be5b83f31a7b921c01399b
Jennifer Martin <jenarther@gmail.com>
2025-06-24 15:34:35 -0700

    a few more minor refactors and moves to the util file

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,37 @@ import { useState } from 'react'
 import './App.css'
 import { GuessDisplay } from './components/GuessDisplay';
 import { UserInput } from './components/UserInput';
-import { translateToColors } from './utils';
-
-const GameStatus = {
-  During: "during",
-  Win: "win",
-  Loss: "loss",
-} as const;
+import { translateToColors, GameStatus, answerLength, maxGuesses } from './utils';
 
 function App({ answer = "" }) {
   console.log(answer)
-  const maxGuesses = 10;
   const [remaining, setRemaining] = useState(maxGuesses);
   const [guesses, setGuesses] = useState(Array().fill(''));
   const [nextGuess, setNextGuess] = useState(Array().fill('R'));
   const [gameStatus, setGameStatus] = useState("during");
   const [results, setResults] = useState(Array().fill(''));
+
+  const updateNextGuess = (index: number, letter: string) => {
+    const updatedInProgressGuess = nextGuess.slice();
+    updatedInProgressGuess[index] = letter;
+    setNextGuess(updatedInProgressGuess);
+  };
+
+  const handleClick = () => {
+    const guessNumber = maxGuesses - remaining;
+    setRemaining((remaining) => remaining - 1);
+
+    const updatedGuesses = guesses.slice();
+    const newGuess = "" + nextGuess[0] + nextGuess[1] + nextGuess[2] + nextGuess[3];
+    updatedGuesses[guessNumber] = newGuess;
+    setGuesses(updatedGuesses);
+
+    checkResult(updatedGuesses[guessNumber]);
+    
+    if (remaining == 1 && gameStatus != GameStatus.Win) {
+      setGameStatus(GameStatus.Loss);
+    }
+  };
 
   const checkResult = (guess: string) => {
     if (guess == answer) {
@@ -27,14 +42,18 @@ function App({ answer = "" }) {
     let almosts = 0;
     let guessArray: string[] = [...guess];
     let answerArray: string[] = [...answer];
-    for (let ind = 0; ind < 4; ind++) {
+    
+    // check for exact matches
+    for (let ind = 0; ind < answerLength; ind++) {
       if (guessArray[ind] == answerArray[ind]) {
         bingos++;
         guessArray[ind] = " ";
         answerArray[ind] = "-";
       }
     }
-    for (let index = 0; index < 4; index++) {
+
+    //exact matches are out of the way, now check for partial matches
+    for (let index = 0; index < answerLength; index++) {
       const foundIndex = answerArray.indexOf(guessArray[index]);
       if (foundIndex > -1) {
         almosts++;
@@ -47,40 +66,20 @@ function App({ answer = "" }) {
     }
   };
 
-  
-  const updateNextGuess = (index: number, letter: string) => {
-    const updatedInProgressGuess = nextGuess.slice();
-    updatedInProgressGuess[index] = letter;
-    setNextGuess(updatedInProgressGuess);
-  };
-
-  const handleClick = () => {
-    const guessNumber = maxGuesses - remaining;
-    const updatedGuesses = guesses.slice();
-    const newGuess = "" + nextGuess[0] + nextGuess[1] + nextGuess[2] + nextGuess[3];
-    updatedGuesses[guessNumber] = newGuess;
-    setGuesses(updatedGuesses);
-    checkResult(updatedGuesses[guessNumber]);
-    setRemaining((remaining) => remaining - 1);
-    if (remaining == 1 && gameStatus != GameStatus.Win) {
-      setGameStatus(GameStatus.Loss);
-    }
-  };
-
   return (
     <>
       <h1>Mastermind</h1>
       <div className="message">
         <h2 hidden={gameStatus != GameStatus.Win}>You won! Refresh the page to play again.</h2>
         <h2 hidden={gameStatus != GameStatus.Loss}>You lost. The answer was {translateToColors(answer)} Refresh the page to play again.</h2>
-        <h4 hidden={gameStatus != GameStatus.During}>Info: "bingo" means the right color in the right spot. "almost" is the right color in the wrong spot.</h4>
+        <h3 hidden={gameStatus != GameStatus.During}>Info: "bingo" means the right color in the right spot. "almost" is the right color in the wrong spot.</h3>
       </div>
       <div className="guessCount">
         <h3>Remaining guesses: {remaining}</h3>
       </div>
       <GuessDisplay guesses={guesses} results={results} />
       <br />
-      <div hidden={gameStatus != "during"}>
+      <div hidden={gameStatus != GameStatus.During}>
         <UserInput clickHandler={handleClick} nextGuessHandler={updateNextGuess} />
       </div>
     </>
